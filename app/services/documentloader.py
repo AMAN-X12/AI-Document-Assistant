689e793b088c1f96223a4805f83805d30c384d97
AMAN-X12 <197904711+AMAN-X12@users.noreply.github.com>
2026-02-12 19:28:26 +0500

    second deployemnt

--- app/services/documentloader.py
+++ app/services/documentloader.py
@@ -3,9 +3,9 @@ import re
 from pathlib import Path
 import tempfile
 import fitz
-import pytesseract
-import io
-from PIL import Image
+# import pytesseract
+# import io
+# from PIL import Image
 from langchain_core.documents import Document
 
 
@@ -30,12 +30,12 @@ def pdf_reader(file_path,file_name):
         text = page.get_text().strip()
         if text:
             doc.append(Document(page_content=clean_text(text), metadata={"file_name":file_name, "page_number": page_num, "source":"text"}))
-        else:
-            pix = page.get_pixmap(dpi=300)
-            img = Image.open(io.BytesIO(pix.tobytes("png")))
-            ocr_text = pytesseract.image_to_string(img)
-            if ocr_text.strip():
-             doc.append(Document(page_content=clean_text(ocr_text), metadata={"source": "ocr", "file_name": file_name, "page_number": page_num}))
+        # else:
+        #     pix = page.get_pixmap(dpi=300)
+        #     img = Image.open(io.BytesIO(pix.tobytes("png")))
+        #     ocr_text = pytesseract.image_to_string(img)
+        #     if ocr_text.strip():
+        #      doc.append(Document(page_content=clean_text(ocr_text), metadata={"source": "ocr", "file_name": file_name, "page_number": page_num}))
     return doc
 
 
